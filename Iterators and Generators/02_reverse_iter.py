853762c1aa0ba0c396b0c7b07f6e9dec1ede6072
darinkirov <darinkirovmid@abv.bg>
2024-04-05 10:44:30 +0300

    Iterators And Generatros
    Iterators And Generatros

---
+++ Iterators and Generators/02_reverse_iter.py
@@ -0,0 +1,15 @@
+class reverse_iter:
+    def __init__(self, iterable):
+        self.iterable = iterable
+        self.index = len(iterable) - 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= 0:
+            result = self.iterable[self.index]
+            self.index -= 1
+            return result
+        else:
+            raise StopIteration
